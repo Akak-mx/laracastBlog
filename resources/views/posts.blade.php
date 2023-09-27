fb7f9206c099c495bc47c361f7f1c9f241d5a1a2
Alejandro Suárez <alesuarezmx@gmail.com>
2023-09-27 14:24:07 -0700

    working on posts sections

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="app.css">
+    <title>My Blog</title>
+</head>
+<body>
+    <article>
+        <a href="post">
+            <h1>My first Post</h1>
+        </a>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nobis explicabo nemo, error voluptatum aliquam cupiditate at dolor distinctio eum quia deserunt vero harum totam, officiis fugiat rerum id consequuntur.
+        </p>
+    </article>
+    <article>
+        <a href="post">
+            <h1>My Second Post</h1>
+        </a>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nobis explicabo nemo, error voluptatum aliquam cupiditate at dolor distinctio eum quia deserunt vero harum totam, officiis fugiat rerum id consequuntur.
+        </p>
+    </article>
+    <article>
+        <a href="post">
+            <h1>My Third Post</h1>
+        </a>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet nobis explicabo nemo, error voluptatum aliquam cupiditate at dolor distinctio eum quia deserunt vero harum totam, officiis fugiat rerum id consequuntur.
+        </p>
+    </article>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => view('posts'));
+
+Route::get('post', fn () => view('post'));
