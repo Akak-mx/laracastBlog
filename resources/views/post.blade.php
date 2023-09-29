a8488513bf888d8afbc96626e0743ca787bc2579
Alejandro Suárez <alesuarezmx@gmail.com>
2023-09-29 14:23:40 -0700

    Switch to blade templates

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
-</head>
-<body>
-    <article>
-        <h1>
-            {{ $post->title }}
-        </h1>
-        <div>
-            {!! $post->body !!}
-        </d>
-    </article>
-    <a href="/">Go Back</a>
-</body>
-</html>
+@extends('layout')
+    @section('content')
+        <article>
+            <h1>
+                {{ $post->title }}
+            </h1>
+            <div>
+                {!! $post->body !!}
+            </d>
+        </article>
+        <a href="/">Go Back</a>
+    @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,24 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
-</head>
-<body>
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
-            <div>
-                {{ $post->excerpt }}
-            </div>
-        </article>
-    @endforeach
-</body>
-</html>
+@extends('layout')
+    @section('content')
+        @foreach ($posts as $post)
+            <article>
+                <h1>
+                    <a href="/posts/{{ $post->slug }}">
+                        {{ $post->title }}
+                    </a>
+                </h1>
+                <div>
+                    {{ $post->excerpt }}
+                </div>
+            </article>
+        @endforeach
+    @endsection
