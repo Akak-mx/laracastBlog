2220e72d0dd63773d182134c2c3433bada4ed613
Alejandro Suárez <alesuarezmx@gmail.com>
2023-09-29 15:00:42 -0700

    Add tweaks and considerations

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
@@ -18,22 +19,24 @@ class Post
 
     public static function all()
     {
-        // return cache()->remember('posts.all', 60, function() {
-            return collect(File::files(resource_path('views/posts')))
-                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-                ->map(fn ($document) => new self(
-                    $document->title,
-                    $document->slug,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body()
-                ))
-                ->sortByDesc('date');
-        // });
+        return cache()->remember('posts.all', 60, fn () => collect(File::files(resource_path('views/posts')))
+            ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn ($document) => new self(
+                $document->title,
+                $document->slug,
+                $document->excerpt,
+                $document->date,
+                $document->body()
+            ))->sortByDesc('date'));
     }
 
     public static function find($slug)
     {
         return self::all()->firstWhere('slug', $slug);
+    }
+
+    public static function findOrFail($slug)
+    {
+        return self::find($slug) ?? throw new ModelNotFoundException;
     }
 }
