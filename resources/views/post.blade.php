1b1b0ead45be5d643e861e12ab2cb52186116e23
Alejandro Suárez <alesuarezmx@gmail.com>
2023-09-29 14:48:03 -0700

    Switch to blade components

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,11 @@
-@extends('layout')
-    @section('content')
-        <article>
-            <h1>
-                {{ $post->title }}
-            </h1>
-            <div>
-                {!! $post->body !!}
-            </d>
-        </article>
-        <a href="/">Go Back</a>
-    @endsection
+<x-layout>
+    <article>
+        <h1>
+            {{ $post->title }}
+        </h1>
+        <div>
+            {!! $post->body !!}
+        </d>
+    </article>
+    <a href="/">Go Back</a>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,14 @@
-@extends('layout')
-    @section('content')
-        @foreach ($posts as $post)
-            <article>
-                <h1>
-                    <a href="/posts/{{ $post->slug }}">
-                        {{ $post->title }}
-                    </a>
-                </h1>
-                <div>
-                    {{ $post->excerpt }}
-                </div>
-            </article>
-        @endforeach
-    @endsection
+<x-layout>
+    @foreach ($posts as $post)
+        <article>
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+</x-layout>
