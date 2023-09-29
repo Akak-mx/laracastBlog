1103e5b3a98fd73c53c00d9d39740fcdfa398786
Alejandro Suárez <alesuarezmx@gmail.com>
2023-09-29 16:34:26 -0700

    Switch to use eloquent model on Post class

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,41 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public function __construct(
-        public $title,
-        public $slug,
-        public $excerpt,
-        public $date,
-        public $body,
-    ) {
-    }
-
-    public static function all()
-    {
-        return cache()->remember('posts.all', 60, fn () => collect(File::files(resource_path('views/posts')))
-            ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-            ->map(fn ($document) => new self(
-                $document->title,
-                $document->slug,
-                $document->excerpt,
-                $document->date,
-                $document->body()
-            ))->sortByDesc('date'));
-    }
-
-    public static function find($slug)
-    {
-        return self::all()->firstWhere('slug', $slug);
-    }
-
-    public static function findOrFail($slug)
-    {
-        return self::find($slug) ?? throw new ModelNotFoundException;
-    }
+    use HasFactory;
 }
--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('posts', ['posts' => Post::all()]));
 
-Route::get('posts/{post}', fn ($slug) => view('post', ['post' => Post::findOrFail($slug)]));
+Route::get('posts/{post}', fn ($id) => view('post', ['post' => Post::findOrFail($id)]));
