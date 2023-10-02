a5fed0774aef06006175f52d400931f69bdb5749
Alejandro Suárez <alesuarezmx@gmail.com>
2023-10-02 16:01:31 -0700

    Add a has many posts relationship to categories

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
             {!! $post->title !!}
         </h1>
         <p>
-            <a href="#">
+            <a href="/categories/{{ $post->$post->category->slug }}">
                 {{ $post->category->name }}
             </a>
         </p>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('posts', ['posts' => Post::get()->sortByDesc('created_at')]));
 
 Route::get('posts/{post:slug}', fn (Post $post) => view('post', ['post' => $post]));
+
+Route::get('/categories/{category:slug}', fn (Category $category) => view('posts', ['posts' => $category->posts]));
