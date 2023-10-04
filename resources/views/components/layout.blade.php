b0016817c9a4ff34bfd90905e5edaac88ba9cad7
Alejandro Suárez <alesuarezmx@gmail.com>
2023-10-04 11:27:43 -0700

    simplify views with components & partials

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,13 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
-</head>
-<body>
-    {{ $slot }}
+<!doctype html>
+
+<title>Laravel From Scratch Blog</title>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+
+<body style="font-family: Open Sans, sans-serif">
+    <section class="px-6 py-8">
+        <nav class="md:flex md:justify-between md:items-center">
+            <div>
+                <a href="/">
+                    <img src="./images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+                </a>
+            </div>
+
+            <div class="mt-8 md:mt-0">
+                <a href="/" class="text-xs font-bold uppercase">Home Page</a>
+
+                <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+                    Subscribe for Updates
+                </a>
+            </div>
+        </nav>
+
+        @include('_header')
+
+        {{ $slot }}
+
+        @include('_footer')
+    </section>
 </body>
-</html>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,19 @@
 <x-layout>
-    @foreach ($posts as $post)
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+    {{-- @foreach ($posts as $post)
         <article>
             <h1>
                 <a href="/posts/{{ $post->slug }}">
@@ -15,5 +29,5 @@
                 {!! $post->excerpt !!}
             </div>
         </article>
-    @endforeach
+    @endforeach --}}
 </x-layout>
