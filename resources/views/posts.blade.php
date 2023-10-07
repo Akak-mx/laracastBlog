5ee4bc0dc18a8c7b9c6665af43f9e8353f825ec1
Alejandro Suárez <alesuarezmx@gmail.com>
2023-10-07 13:42:13 -0700

    Add dropdown component using alpine js

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,6 @@
 <x-layout>
+    @include('_header')
+
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
         @if ($posts->count())
--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,17 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('posts', ['posts' => Post::latest()->with(['category', 'author'])->get()]));
+Route::get('/', fn () => view('posts', [
+    'posts' => Post::latest()->with(['category', 'author'])->get(),
+    'categories' => Category::all(),
+]));
 
 Route::get('posts/{post:slug}', fn (Post $post) => view('post', ['post' => $post]));
 
-Route::get('/categories/{category:slug}', fn (Category $category) => view('posts', ['posts' => $category->posts->load(['category', 'author'])]));
+Route::get('/categories/{category:slug}', fn (Category $category) => view('posts', [
+    'posts' => $category->posts->load(['category', 'author']),
+    'currentCategory' => $category,
+    'categories' => Category::all(),
+]));
 
 Route::get('/authors/{author:username}', fn (User $author) => view('posts', ['posts' => $author->posts->load(['category', 'author'])]));
