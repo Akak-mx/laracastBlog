0ea869684ddea87f4ad2deb6ef391647e785581a
Alejandro Suárez <alesuarezmx@gmail.com>
2023-10-08 20:24:02 -0700

    Fix queryScope filter to search adecuately

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,8 +16,10 @@ class Post extends Model
     public function scopeFilter($query, array $filters)
     {
         $query->when($filters['search'] ?? false, fn ($query, $search) => $query
-            ->where('title', 'like', '%'.$search.'%')
-            ->OrWhere('body', 'like', '%'.$search.'%')
+            ->where(fn ($query) => $query
+                ->where('title', 'like', '%'.$search.'%')
+                ->OrWhere('body', 'like', '%'.$search.'%')
+            )
         );
 
         $query->when($filters['category'] ?? false, fn ($query, $category) => $query
