1f1b9fbb17d3897dacd1b81e690b93266dbdd2da
Alejandro Suárez <alesuarezmx@gmail.com>
2023-10-08 20:14:41 -0700

    search multiple queries

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -15,7 +15,7 @@
     </x-dropdown-item>
     @foreach ($categories as $category)
         <x-dropdown-item
-            href="/?category={{ $category->slug }}"
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
             :active='request()->is("categories/{$category->slug}")'
         >
             {{ ucwords($category->name) }}
