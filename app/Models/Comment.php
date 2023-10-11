eddeac3fb0b768b93e7ab8fddbe048ef59189c8c
Alejandro Suárez <alesuarezmx@gmail.com>
2023-10-11 16:42:51 -0700

    Display comments dynamic

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
