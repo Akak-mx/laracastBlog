985ab4d5aafe52b8d277089bda021deeed30a66b
Alejandro Suárez <alesuarezmx@gmail.com>
2023-10-11 16:09:32 -0700

    Generate comment model and structure migrations

--- database/migrations/2023_10_11_230414_create_comments_table.php
+++ database/migrations/2023_10_11_230414_create_comments_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id');
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->string('title');
-            $table->string('slug');
-            $table->text('excerpt');
             $table->text('body');
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 };
