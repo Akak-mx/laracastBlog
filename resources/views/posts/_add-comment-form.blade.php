dacfb36aff69ee19f00998a1a2d256bb3d28ba7d
Alejandro Suárez <alesuarezmx@gmail.com>
2023-10-12 14:21:41 -0700

    Display validation errors and refactor to add components

---
+++ resources/views/posts/_add-comment-form.blade.php
@@ -0,0 +1,26 @@
+@auth
+    <x-panel>
+        <form method="POST" action="/posts/{{ $post->slug }}/comments">
+            @csrf
+            <header class="flex items-center">
+                <img src="https://i.pravatar.cc/100/?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
+                <h2 class="ml-4">
+                    Want to participate
+                </h2>
+            </header>
+            <div class="mt-6">
+                <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" rows="5" placeholder="Quick, say something!" required></textarea>
+                @error('name')
+                    <span class="text-xs text-red-500 mt-1">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                <x-submit-button>POST</x-submit-button>
+            </div>
+        </form>
+    </x-panel>
+@else
+    <p class="font-semibold">
+        <a class="hover:underline" href="/register">Regitesr</a> or <a class="hover:underline" href="/login">log in</a> to leave a comment.
+    </p>
+@endauth
