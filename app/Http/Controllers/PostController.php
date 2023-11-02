c1d601d08c1534b216be4f9bf440545e47170cf7
Alejandro Suárez <alesuarezmx@gmail.com>
2023-11-02 16:55:20 -0700

    Store post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Validation\Rule;
 
 class PostController extends Controller
 {
@@ -23,5 +24,22 @@ class PostController extends Controller
     public function create()
     {
         return view('posts.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'title' => ['required'],
+            'slug' => ['required', Rule::unique('posts', 'slug')],
+            'excerpt' => ['required'],
+            'body' => ['required'],
+            'category_id' => ['required', Rule::exists('categories', 'id')],
+        ]);
+
+        $attributes['user_id'] = auth()->id();
+
+        $post = Post::create($attributes);
+
+        return redirect("/posts/{$post->slug}")->with('success', 'Post Created!');
     }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,100 @@
 <x-layout>
     <section class="px-6 py-8">
-        Hello
+        <form action="/admin/posts" method="POST">
+            @csrf
+
+            <x-panel class="max-w-sm mx-auto">
+                <div class="mb-6">
+                    <label for="title" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                        Title
+                    </label>
+                    <input
+                        class="border border-gray-400 p-2 w-full"
+                        type="text"
+                        name="title"
+                        id="title"
+                        value="{{ old('title')}}"
+                        required
+                    >
+                    @error('title')
+                        <span class="text-xs text-red-500 mt-1">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label for="slug" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                        Slug
+                    </label>
+                    <input
+                        class="border border-gray-400 p-2 w-full"
+                        type="text"
+                        name="slug"
+                        id="slug"
+                        value="{{ old('slug') }}"
+                        required
+                    >
+                    @error('slug')
+                        <span class="text-xs text-red-500 mt-1">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label for="excerpt" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                        Excerpt
+                    </label>
+                    <textarea
+                        class="border border-gray-400 p-2 w-full"
+                        name="excerpt"
+                        id="excerpt"
+                        required
+                    >{{ old('excerpt') }}
+                    </textarea>
+                    @error('textarea')
+                        <span class="text-xs text-red-500 mt-1">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label for="body" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                        Body
+                    </label>
+                    <textarea
+                        class="border border-gray-400 p-2 w-full"
+                        name="body"
+                        id="body"
+                        required
+                    >{{ old('body') }}
+                    </textarea>
+                    @error('textarea')
+                        <span class="text-xs text-red-500 mt-1">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label for="category_id" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                        Category
+                    </label>
+                    <select name="category_id" id="category_id">
+                        @php
+                            $categories = App\Models\Category::all();
+                        @endphp
+                        @foreach ($categories as $category)
+                            <option
+                                value="{{ $category->id}}"
+                                {{ old('category_id') === $category->id ? 'selected' : '' }}
+                            >
+                                {{ ucwords($category->name) }}
+                            </option>
+                        @endforeach
+                    </select>
+                    @error('category_id')
+                        <span class="text-xs text-red-500 mt-1">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <x-submit-button>Publish</x-submit-button>
+
+            </x-panel>
+        </form>
     </section>
 </x-layout>
